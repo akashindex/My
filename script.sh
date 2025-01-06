503588b992b291d61b351817475b5aed78d39626
akashindex <itz.asdf303@gmail.com>
2025-01-06 19:54:10 +0500

    changes

--- script.sh
+++ script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Start and end dates
-start_date="2020-08-03 00:00:00"
-end_date="2020-08-05 00:00:00"
+start_date="2022-08-03 00:00:00"
+end_date="2024-08-05 00:00:00"
 
 # Define a list of random words for commit messages
 words=("refactor" "fix" "update" "optimize" "cleanup" "enhance" "test" "improve" "debug" "add" "remove" "merge")
 
 # Convert start_date and end_date to seconds since epoch
-start_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$start_date" +"%s")
-end_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$end_date" +"%s")
+start_date_epoch=$(date -d "$start_date" +"%s")
+end_date_epoch=$(date -d "$end_date" +"%s")
 
 # Set the total number of commits to make (adjustable)
-total_commits=10
+total_commits=500
 
 for i in $(seq 1 $total_commits); do
     # Generate a random timestamp between start_date and end_date
     random_date_epoch=$((start_date_epoch + RANDOM % (end_date_epoch - start_date_epoch + 1)))
-    random_date=$(date -j -f "%s" "$random_date_epoch" +"%Y-%m-%d %H:%M:%S")
+    random_date=$(date -d "@$random_date_epoch" +"%Y-%m-%d %H:%M:%S")
 
     # Select a random word for the commit message
     random_word=${words[$RANDOM % ${#words[@]}]}
@@ -27,7 +27,7 @@ for i in $(seq 1 $total_commits); do
     git add commit_log.txt
 
     # Commit with the random date and message
-    GIT_AUTHOR_DATE="$random_date" GIT_COMMITTER_DATE="$random_date" git commit --no-edit --date "$random_date" -m "$random_word"
+    GIT_AUTHOR_DATE="$random_date" GIT_COMMITTER_DATE="$random_date" git commit --no-edit --date="$random_date" -m "$random_word"
 done
 
 # Push all commits to the repository at once
